6f50efb2a79460bc6c2e4416858c02208bd61a33
aelmod <aelmod@mail.ru>
2017-05-20 18:32:35 +0300

    added ArrayUtil

--- src/main/java/com/github/aelmod/dynamic/medium/App.java
+++ src/main/java/com/github/aelmod/dynamic/medium/App.java
@@ -1,10 +1,11 @@
 package com.github.aelmod.dynamic.medium;
 
-public class MaxValueOfArray {
+public class App {
 
     public static void main(String[] args) {
         int[] arr = new int[]{0, 1, 2, 3, 4, 5, 6, 10, 23, 1, 2, 3, 54, 23, 123, 34, 123, 0, -4, 324, 1, 32, 23};
-        System.out.println(getMaxValue(arr));
+        ArrayUtil arrayUtil = new ArrayUtil();
+        System.out.println(arrayUtil.getIndexOfMaxValueOfArray(arr));
     }
 
 
--- src/main/java/com/github/aelmod/dynamic/medium/ArrayUtil.java
+++ src/main/java/com/github/aelmod/dynamic/medium/ArrayUtil.java
@@ -1,7 +1,27 @@
 package com.github.aelmod.dynamic.medium;
 
-/**
- * Created by aelmod on 20.05.17.
- */
 public class ArrayUtil {
+
+    public int getMaxValueOfArray(int[] arr) {
+        int currentMax = arr[0];
+
+        for (int i : arr) {
+            if (currentMax < i) currentMax = i;
+        }
+        return currentMax;
+    }
+
+    public int getIndexOfMaxValueOfArray(int[] arr) {
+        int currentMaxIndex = 0;
+        int currentMax = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > currentMax) {
+                currentMax = arr[i];
+                currentMaxIndex = i;
+            }
+        }
+
+        return currentMaxIndex;
+    }
 }
